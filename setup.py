1115f484d1f5ad927f7789a2e44726f9aef19c08
Brian Hatfield <bmhatfield@gmail.com>
2013-05-14 14:23:47 -0400

    put script in nagios path

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
+nagios_path = '/usr/lib/nagios/plugins'
+
 setup(name='nagios-plugin-mongodb',
       version='0.1.0',
       description='A script to monitor MongoDB with nagios-compatible output',
       author='Mike Zupan',
       author_email='mike@zcentric.com',
       url='https://github.com/bmhatfield/nagios-plugin-mongodb',
-      py_modules=['check_mongodb'],
+      data_files=[nagios_path, 'check_mongodb.py'],
       license='Retribution'
      )
